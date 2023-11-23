cdc7e562a4a7772fbfeb7411cce2e59d811bc36a
remdambrosio <rda7@shaw.ca>
2023-11-23 11:08:50 -0800

    added support for XP

--- Pokemon.py
+++ Pokemon.py
@@ -2,8 +2,8 @@ class Pokemon:
     def __init__(self, poke_got):
         self.name = poke_got["name"]
         self.id = poke_got["id"]
+        self.xp = poke_got["base_experience"]
         self.types = [typ["type"]["name"] for typ in poke_got["types"]]
-        
         self.height = poke_got["height"] / 10
         self.weight = poke_got["weight"] / 10
     
@@ -13,6 +13,7 @@ class Pokemon:
 
         return f"Name: {self.name}\n"\
             f"ID: {self.id}\n"\
+            f"XP: {self.xp}\n"\
             f"Types: {', '.join(self.types)}\n"\
             f"Height: {self.height} m\n"\
             f"Weight: {self.weight} kg"
