dceae3b6edc6a15003ec30927c91be22126acb7a
remdambrosio <rda7@shaw.ca>
2023-12-12 13:27:51 -0800

    implemented csv import

--- Population.py
+++ Population.py
@@ -1,3 +1,16 @@
+import pandas as pd
+
 class Population:
-    def __init__(self, data):
-        
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = None
+        self.load()
+
+    def load(self):
+        try:
+            self.data = pd.read_csv(self.file_path)
+        except FileNotFoundError:
+            print(f"Error: {self.file_path} not found. Population data could not be accessed.")
+
+    def print(self):
+            print(self.data.head())
