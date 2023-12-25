5e09533b626b69b76183a6db8019de50386ee208
Rem D'Ambrosio <rda7@shaw.ca>
2023-12-25 13:35:01 -0800

    fixed type hints

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from Population import Population
 def get_poke(user_poke_name: str) -> Pokemon:
     """Takes the user-entered pokemon name and creates a corresponding Pokemon object
     """
-    poke_url = "https://pokeapi.co/api/v2/pokemon/" + user_poke_name  # Use input to define appropriate URL
-    poke_request = requests.get(poke_url)  # Set up request
-    poke_got = "0"  # Give request a placeholder value
+    poke_url: str = "https://pokeapi.co/api/v2/pokemon/" + user_poke_name  # Use input to define appropriate URL
+    poke_request: requests.Response = requests.get(poke_url)  # Set up request
+    poke_got: typing.Union[str, dict] = "0"  # Give request a placeholder value
     while poke_got == "0":
         try:  # Try to get json for that pokemon
             poke_request.raise_for_status()
@@ -19,7 +19,7 @@ def get_poke(user_poke_name: str) -> Pokemon:
         except requests.exceptions.RequestException as req_ex:
             if poke_request.status_code == 404:  # If a 404 error occurs, that pokemon probably isn't real; ask for pokemon name again
                 print("That's not a real Pokemon. Not yet, anyway.")
-                user_poke = input("Enter the name of a REAL Pokemon: ").lower()
+                user_poke: str = input("Enter the name of a REAL Pokemon: ").lower()
                 poke_url = "https://pokeapi.co/api/v2/pokemon/" + user_poke
                 poke_request = requests.get(poke_url)
             else:
@@ -33,8 +33,8 @@ pop = Population("Population_Data.csv")
 
 # Ask user for a pokemon name, then use PokeAPI to create an appropriate Pokemon object
 
-user_poke_name = input("Enter the name of a Pokemon: ").lower()  # Ask for a pokemon name
-user_poke = get_poke(user_poke_name)  # Create a corresponding Pokemon object
+user_poke_name: str = input("Enter the name of a Pokemon: ").lower()  # Ask for a pokemon name
+user_poke: Pokemon = get_poke(user_poke_name)  # Create a corresponding Pokemon object
 
 # Test prints
 
